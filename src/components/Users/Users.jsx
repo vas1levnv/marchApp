ea014e427f48fd7e5a47d99562dffe18d8d6dfd2
vas1levnv <vas1levnv@yandex.ru>
2022-03-28 20:19:22 +0300

    51

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,34 +1,13 @@
 import s from './Users.module.css'
+import * as axios from "axios";
+import userPhoto from '../../assets/images/no-avatar.png'
 
 const Users = (props) => {
-
     if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl: 'https://lumpics.ru/wp-content/uploads/2017/11/Programmyi-dlya-sozdaniya-avatarok.png',
-                followed: false,
-                fullName: 'Dmitry',
-                status: 'i m a boss',
-                location: {city: 'Minsk', country: 'Belarus'}
-            },
-            {
-                id: 2,
-                photoUrl: 'https://lumpics.ru/wp-content/uploads/2017/11/Programmyi-dlya-sozdaniya-avatarok.png',
-                followed: true,
-                fullName: 'Sasha',
-                status: 'i m a boss too',
-                location: {city: 'Moscow', country: 'Russia'}
-            },
-            {
-                id: 3,
-                photoUrl: 'https://lumpics.ru/wp-content/uploads/2017/11/Programmyi-dlya-sozdaniya-avatarok.png',
-                followed: false,
-                fullName: 'Andrew',
-                status: 'i m a boss too',
-                location: {city: 'Kiev', country: 'Ukraine'}
-            },]
-        )
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                props.setUsers(response.data.items)
+            })
     }
 
     return (<div>
@@ -36,7 +15,7 @@ const Users = (props) => {
                 props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
-                            <img src={u.photoUrl} className={s.photo}/>
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto} className={s.photo}/>
                         </div>
                         <div>
                             {u.followed
@@ -51,12 +30,12 @@ const Users = (props) => {
                     </span>
                     <span>
                         <span>
-                            <div>{u.fullName}</div>
+                            <div>{u.name}</div>
                             <div>{u.status}</div>
                         </span>
                         <span>
-                            <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                            <div>{'u.location.city'}</div>
+                            <div>{'u.location.country'}</div>
                         </span>
                     </span>
                 </div>)
--- src/index.js
+++ src/index.js
@@ -8,11 +8,13 @@ import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
 ReactDOM.render(
+    <React.StrictMode>
     <BrowserRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>,
+    </BrowserRouter>
+    </React.StrictMode>,
     document.getElementById('root')
 );
 
