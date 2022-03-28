85e7bba0501383e64e9b81f4d42c8c860ba44cbd
vas1levnv <vas1levnv@yandex.ru>
2022-03-28 20:30:35 +0300

    52

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,14 +3,16 @@ import * as axios from "axios";
 import userPhoto from '../../assets/images/no-avatar.png'
 
 const Users = (props) => {
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
-            .then(response => {
-                props.setUsers(response.data.items)
-            })
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => {
+                    props.setUsers(response.data.items)
+                })
+        }
     }
-
     return (<div>
+            <button onClick={getUsers}>Get users</button>
             {
                 props.users.map(u => <div key={u.id}>
                     <span>
@@ -34,7 +36,7 @@ const Users = (props) => {
                             <div>{u.status}</div>
                         </span>
                         <span>
-                            <div>{'u.location.city'}</div>
+                            <div>{u.location.city}</div>
                             <div>{'u.location.country'}</div>
                         </span>
                     </span>
