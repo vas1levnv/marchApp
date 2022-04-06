8fa897a1e614fba12d09046e15e166b699bc4fc2
vas1levnv <vas1levnv@yandex.ru>
2022-04-06 18:28:59 +0300

    61

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import s from './Header.module.css'
+import {NavLink} from "react-router-dom";
 
 const Header = (props) => {
     return(
         <header className={s.header}>
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/480px-Facebook_Logo_%282019%29.png" alt=""/>
+       <div className={s.loginBlock}>
+           {props.isAuth ? props.login
+               : <NavLink to={'/login'}>Login</NavLink>}
+       </div>
         </header>
     )
 }
