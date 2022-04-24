b4971ab20d1689f60e488d9af83bcdb252f79070
vas1levnv <vas1levnv@yandex.ru>
2022-04-24 21:53:20 +0300

    79

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,6 +5,7 @@ import {required} from "../../utils/validators/validators";
 import {connect} from "react-redux";
 import {login} from "../../redux/auth-reducer";
 import {Navigate} from "react-router-dom";
+import style from '../common/FormsControls/FormsControls.module.css'
 
 const LoginForm = (props) => {
     return (
@@ -23,6 +24,9 @@ const LoginForm = (props) => {
                 <Field name={"rememberMe"} type={"checkbox"}
                        component={Input}/> remember me
             </div>
+            {props.error && <div className={style.formSummaryError}>
+                {props.error}
+            </div>}
             <div>
                 <button>Login</button>
             </div>
@@ -38,13 +42,13 @@ const Login = (props) => {
         props.login(formData.email, formData.password, formData.rememberMe);
     }
 
-    if (props.isAuth){
+    if (props.isAuth) {
         return <Navigate to={"/profile"}/>
     }
 
     return (<div>
             <h1>Login</h1>
-            <LoginReduxForm onSubmit={onSubmit} />
+            <LoginReduxForm onSubmit={onSubmit}/>
         </div>
     )
 }
@@ -53,4 +57,4 @@ const mapStateToProps = (state) => ({
     isAuth: state.auth.isAuth
 })
 
-export default connect(mapStateToProps,{login})(Login)
+export default connect(mapStateToProps, {login})(Login)
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -11,7 +11,7 @@ class ProfileContainer extends React.Component {
     componentDidMount() {
         let userId = this.props.router.params.userId
         if (!userId) {
-            userId = 19888
+            userId = this.props.authorizedUserId
         }
         this.props.getUserProfile(userId)
         this.props.getStatus(userId)
@@ -31,7 +31,9 @@ class ProfileContainer extends React.Component {
 
 const mapStateToProps = (state) => ({
     profile: state.profilePage.profile,
-    status: state.profilePage.status
+    status: state.profilePage.status,
+    authorizedUserId: state.auth.userId,
+    isAuth: state.auth.isAuth
 })
 
 function withRouter(Component) {
@@ -53,5 +55,5 @@ function withRouter(Component) {
 export default compose(
     connect(mapStateToProps, {getUserProfile, getStatus, updateStatus}),
     withRouter,
-    withAuthRedirect
+   // withAuthRedirect
 )(ProfileContainer)
